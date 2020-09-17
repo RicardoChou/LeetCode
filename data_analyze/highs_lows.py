a09452b0eb1adafd7d6b9b30b655ccd7d89f271f
RicardoChou <244250380@qq.com>
2020-09-17 10:38:44 +0800

    ENTER

--- data_analyze/highs_lows.py
+++
@@ -1,9 +0,0 @@
-import csv
-filename = 'data_analyze\data\sitka_weather_2018_simple.csv'
-with open(filename) as f:
-    reader = csv.reader(f)
-    header_row = next(reader)
-    stations = []
-    for row in reader:
-        stations.append(row[0])
-    print(stations)
