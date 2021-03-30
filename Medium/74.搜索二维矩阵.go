7ca0e1264bf37777c1e51dfe3de52de5b4d3cd75
RicardoChou <244250380@qq.com>
2021-03-30 09:18:22 +0800

    2021/3/30

---
+++ Medium/74.搜索二维矩阵.go
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=74 lang=golang
+ *
+ * [74] 搜索二维矩阵
+ */
+
+// @lc code=start
+func searchMatrix(matrix [][]int, target int) bool {
+	row, col := 0, len(matrix[0])-1
+	for row < len(matrix) && col >= 0 {
+		if matrix[row][col] == target {
+			return true
+		} else if matrix[row][col] < target {
+			row++
+		} else {
+			col--
+		}
+	}
+	return false
+}
+
+// @lc code=end
+
