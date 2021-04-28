5cb55cb9773d55dd91889d4e5bff40d292a83a5f
RicardoChou <244250380@qq.com>
2021-04-28 09:43:49 +0800

    2021/4/28

---
+++ Easy/938.二叉搜索树的范围和.go
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=938 lang=golang
+ *
+ * [938] 二叉搜索树的范围和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func rangeSumBST(root *TreeNode, low int, high int) int {
+	if root == nil {
+		return 0
+	}
+	if root.Val < low {
+		return rangeSumBST(root.Right, low, high)
+	}
+	if root.Val > high {
+		return rangeSumBST(root.Left, low, high)
+	}
+	return root.Val + rangeSumBST(root.Left, low, high) + rangeSumBST(root.Right, low, high)
+}
+
+// @lc code=end
+
