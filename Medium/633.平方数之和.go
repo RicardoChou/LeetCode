d26f768a8a56e0646db261bb3795c6e939ff1d3d
RicardoChou <244250380@qq.com>
2021-04-28 09:29:04 +0800

    2021/4/28

--- Medium/633.平方数之和.go
+++ Medium/633.平方数之和.go
@@ -5,12 +5,14 @@
  */
 
 // @lc code=start
+// 双指针：左指针从0开始右移，右指针从根号c开始左移
 func judgeSquareSum(c int) bool {
-	for i, j := 0, int(math.Sqrt(float64(c))); i <= j; {
-		num := i*i + j*j
-		if num == c {
+	i, j := 0, int(math.Sqrt(float64(c)))
+	for i <= j {
+		sum := i*i + j*j
+		if sum == c {
 			return true
-		} else if num < c {
+		} else if sum < c {
 			i++
 		} else {
 			j--
